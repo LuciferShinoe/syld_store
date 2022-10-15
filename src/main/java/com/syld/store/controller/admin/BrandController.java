a67594348bf4354ab107127b814a025225cd3831
035201000354 <hieu.dbutcl@gmail.com>
2022-10-15 13:30:07 +0700

    done

--- src/main/java/com/syld/store/controller/admin/BrandController.java
+++ src/main/java/com/syld/store/controller/admin/BrandController.java
@@ -2,9 +2,17 @@ package com.syld.store.controller.admin;
 
 import com.syld.store.controller.BaseController;
 import com.syld.store.services.brand.BrandService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping(path = "/admin/brands")
 public class BrandController extends BaseController {
 
     private final BrandService brandService;
@@ -12,8 +20,12 @@ public class BrandController extends BaseController {
     @GetMapping
     public String GetByPage(Model model){
         try {
-            model.addAtribute("brands", )
+            model.addAttribute("brands", brandService.getClass());
+        }catch (Exception e){
+            log.info(e.getMessage());
         }
+        return view(model, "List Brand", "brand/list", this.admin_layout );
+
     }
 
 }
